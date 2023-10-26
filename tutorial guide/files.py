2a7b8fa41d1243a002b0be76a105477c4b7a4fd4
unknown <ronaldomano7070@gmail.com>
2023-10-26 10:07:21 +0530

    third commit

---
+++ tutorial guide/files.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI(
+    title="Upload file",
+    description="This is just simple code to upload files in fastapi server"
+)
+
+MAX_FILE_SIZE = 1024 # 10 MB
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(max_size=MAX_FILE_SIZE)):
+    # Save the uploaded file to the filesystem
+    with open(f"uploads/{file.filename}", "wb") as f:
+        content = await file.read()
+        f.write(content)
+
+    return {"filename": file.filename}
